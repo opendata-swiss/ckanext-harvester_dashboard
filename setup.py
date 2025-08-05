6f71ab86f07e2421c9198f8535b1311dedeed6d3
Stefanie Taepke <stefina@users.noreply.github.com>
2025-08-05 14:24:56 +0200

    doc: update Python versions

--- setup.py
+++ setup.py
@@ -29,7 +29,10 @@ setup(
         "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
-        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
     # What does your project relate to?
     keywords="""CKAN harvester dashboard""",
